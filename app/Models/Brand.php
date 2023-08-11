8325fb46280055cbbc879693873c2ad903d1d7d3
n1mah <nima.heidari.n@gmail.com>
2023-08-11 16:39:24 +0330

    added relation model (category,brand)

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Brand extends Model
 {
     use HasFactory;
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
     protected $fillable = [
         'title',
         'title_en',
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function brands(): HasMany
+    {
+        return $this->hasMany(Brand::class);
+    }
+
     protected $fillable = [
         'title',
         'title_en',
