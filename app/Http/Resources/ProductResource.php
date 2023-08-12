888190f6b0224c1278b3967c32c7c77b49585b2f
n1mah <nima.heidari.n@gmail.com>
2023-08-12 08:17:33 +0330

    resource added - Product

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'brand_id'=>$this->brand_id,
+            'title'=>$this->title,
+            'title_en'=>$this->title_en,
+            'description'=>$this->description,
+            'period'=>$this->period,
+            'period_title'=>$this->period_title,
+            'price'=>$this->price,
+        ];
+    }
+}
