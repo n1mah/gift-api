379989bc2d51aef8e312b8be9c3decc4b2fb37a3
n1mah <nima.heidari.n@gmail.com>
2023-08-12 11:02:21 +0330

    store request implement - product

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -11,7 +11,7 @@ class StoreProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,13 @@ class StoreProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title'=>"required|string|max:127",
+            'brand_id'=>"required|exists:brands,id",
+            'title_en'=>"required|string|max:127",
+            'description'=>"required|string|max:127",
+            'period'=>"required|string|max:127",
+            'period_title'=>"required|string|max:127",
+            'price'=>"required|string|max:127",
         ];
     }
 }
