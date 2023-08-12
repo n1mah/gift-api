46aa140cad6c2b0d458285c1388c6eed999edfc0
n1mah <nima.heidari.n@gmail.com>
2023-08-12 11:04:40 +0330

    controller implement - product

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -5,40 +5,26 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        return ProductResource::collection(Product::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreProductRequest $request)
     {
-        //
+        $product =  Product::create($request->validated());
+        return ProductResource::make($product);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Product $product)
     {
-        //
+        return ProductResource::make($product);
     }
 
     /**
