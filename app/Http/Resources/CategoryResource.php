7544d54b8433502fcdf1573dbb04bedb2c9cf7a3
n1mah <nima.heidari.n@gmail.com>
2023-08-12 11:00:44 +0330

    resource changed - category

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,6 +15,7 @@ class CategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'=>$this->id,
             'title'=>$this->title,
             'title_en'=>$this->title_en,
         ];
